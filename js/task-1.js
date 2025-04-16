2141b41d3ee9710d8952c5f9d124ce1ce9eec493
imyroslav <imyroslav@ukr.net>
2025-04-17 00:25:54 +0700

    task-1-update-3

--- js/task-1.js
+++ js/task-1.js
@@ -5,4 +5,21 @@ const categoryArray = document.querySelectorAll(".item");
 console.log(`Number of categories:${categoryArray.length}`);
 console.log(categoryArray);
 
+categoryArray.forEach(catData);
+
+function catData(listItem) {
+    
+    const headingTwo = document.querrySelector("h2");
+    console.log(headingTwo.textContent)
+
+}
+
+
+
+
+
+
+
+
+
 
